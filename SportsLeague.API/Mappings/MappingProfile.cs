7b2bea85a57b35e417d134868663c6319e872955
acevedoBrandon <brandonacevedo012@outlook.com>
2026-04-02 10:15:54 -0500

    Se añadió la Entity Sponsor y su configuración inicial en el DbContext

--- SportsLeague.API/Mappings/MappingProfile.cs
+++ SportsLeague.API/Mappings/MappingProfile.cs
@@ -12,5 +12,24 @@ public class MappingProfile : Profile
         // Team mappings
         CreateMap<TeamRequestDTO, Team>();
         CreateMap<Team, TeamResponseDTO>();
+
+        // Player mappings
+        CreateMap<PlayerRequestDTO, Player>();
+        CreateMap<Player, PlayerResponseDTO>()
+        .ForMember(
+        dest => dest.TeamName,
+        opt => opt.MapFrom(src => src.Team.Name));
+
+        // Referee mappings
+        CreateMap<RefereeRequestDTO, Referee>();
+        CreateMap<Referee, RefereeResponseDTO>();
+
+        // Tournament mappings
+        CreateMap<TournamentRequestDTO, Tournament>();
+        CreateMap<Tournament, TournamentResponseDTO>()
+        .ForMember(
+        dest => dest.TeamsCount,
+        opt => opt.MapFrom(src =>
+        src.TournamentTeams != null ? src.TournamentTeams.Count : 0));
     }
 }
